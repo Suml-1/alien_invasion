54fd9de906627c45ad7419b570938dec5b9d7971
Suml <2906247040@qq.com>
2025-11-22 22:19:39 +0800

    添加 Alien Invasion 项目源代码文件

---
+++ alien_invasion/settings.py
@@ -0,0 +1,59 @@
+class Settings:
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        # 武装飞船的速度控制
+        self.ship_speed = 0.8
+        # 子弹设置
+        self.bullet_speed = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+        # 外星人设置
+        self.alien_speed = 0.2
+        self.fleet_drop_speed = 10  #舰队向下移动的速度
+        self.fleet_direction = 1  #1表示向右移动，-1向左移动
+        # 玩家所拥有的飞船(命)
+        self.ship_limit = 3
+
+        # 加快游戏节奏的速度
+        self.speedup_scale = 1.1
+        # 分数的提升速度
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+        # 得分
+        self.alien_points = None
+
+
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed = 0.8
+        self.bullet_speed = 0.5
+        self.alien_speed = 0.2
+        self.fleet_direction = 1  # 1表示向右移动，-1向左移动
+        self.alien_points = 50
+
+
+    def increase_speed(self):
+        """游戏难度及得分提升"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+
+
+
+
+
+
+
+
+
+
